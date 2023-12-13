09b540467a01fa5c6bb9e05a2af1bc3d14482e3d
gabriel <gabrieloficial1567@gmail.com>
2023-12-13 20:57:37 -0300

    Subindo Js

--- js/script.js
+++ js/script.js
@@ -48,11 +48,100 @@ function infosClima(cordenadas){
     })
 
     .then(data => {
-        console.log(data)
+        local_clima = data.location
+        informacoes_clima = data.timelines
+
+        setTimeout(() => {
+            pegaInfoEndereco(local_clima)
+            montaInfoClima(informacoes_clima)
+        }, 1000);
+    })
+
+    .catch(error => {
+        console.error("Erro: [ ", error, " ]")
+    })
+    
+}
+
+
+function pegaInfoEndereco(info_endereco){
+
+    let latitude = info_endereco.lat
+    let longitude = info_endereco.lon
+    let cord = latitude + "+" + longitude
+
+    let pega_info_endereco = url_api_opencage + "q=" + cord + api_key_open_cage
+
+    fetch(pega_info_endereco).then( response => {
+        if(!response.ok){
+            throw new Error(`Erro na requisição pega Info Endereco: ${response.status}`)
+        }
+
+        return response.json()
+    })
+
+    .then( data => {
+        infos_endereco = data.results[0].components
+        montaEndereco(infos_endereco)
     })
 
     .catch(error => {
         console.error("Erro: [ ", error, " ]")
     })
 
+}
+
+function montaEndereco(infos){
+
+    let city = infos.city
+    let estado = infos.state
+    let pais = infos.country
+
+    document.getElementById("name_city").innerText = city
+    document.getElementById("name_estado").innerText = estado
+    document.getElementById("name_pais").innerText = pais
+
+    document.querySelector(".infos_endereco").classList.remove("d-none")
+}
+
+function montaInfoClima(infos){
+    let daily = infos.daily
+
+    for (let index = 0; index < daily.length; index++) {
+
+        if(index == 0){
+            const info_diaria = daily[index]
+            criaCardInicial(info_diaria)
+        }
+        
+    }
+
+}
+
+function criaCardInicial(data){
+
+    let time = new Date(data.time)
+    time = time.toLocaleDateString('pt-BR', { weekday: 'long' })
+    
+    const values  = data.values
+
+
+    let dia = document.querySelector("#box_dia")
+    dia.innerText = time
+
+    // Temperatura //
+    const temp_media = document.querySelector("#box_temperatura .valor")
+    temp_media.innerText = typeof values.temperatureAvg !== "undefined" ? values.temperatureAvg : ""
+
+    const temp_max = document.querySelector("#max_temperatura")
+    temp_max.innerText = typeof values.temperatureMax !== "undefined" ? values.temperatureMax : ""
+
+    const temp_min = document.querySelector("#min_temperatura")
+    temp_max.innerText = typeof values.temperatureMin !== "undefined" ? values.temperatureMin : ""
+
+    // Vento //
+    const humidade = document.querySelector("#box_umidade .valor")
+    humidade.innerText = typeof values.humidityAvg !== "undefined" ? values.humidityAvg : ""
+
+    //  //
 }
